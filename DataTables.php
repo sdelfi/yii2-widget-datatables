2bdd34fb45fd36e232010659fa21b5e623c3e331
Delfi89 <delfi89@gmail.com>
2018-08-31 14:54:27 +0300

    Update old bower libs

--- DataTables.php
+++ DataTables.php
@@ -48,14 +48,13 @@ class DataTables extends \yii\grid\GridView
         $view = $this->getView();
         $id = $this->tableOptions['id'];
 
-        //Bootstrap3 Asset by default
+        //Bootstrap4 Asset by default
         DataTablesBootstrapAsset::register($view);
 
         //TableTools Asset if needed
         if (isset($clientOptions["tableTools"]) || (isset($clientOptions["dom"]) && strpos($clientOptions["dom"], 'T') >= 0)) {
-            $tableTools = DataTablesTableToolsAsset::register($view);
-            //SWF copy and download path overwrite
-            $clientOptions["tableTools"]["sSwfPath"] = $tableTools->baseUrl . "/swf/copy_csv_xls_pdf.swf";
+            DataTablesButtonsBs4Asset::register($view);
+            DataTablesSelectBs4Asset::register($view);
         }
         $options = Json::encode($clientOptions);
         $view->registerJs("jQuery('#$id').DataTable($options);");
